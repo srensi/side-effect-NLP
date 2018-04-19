a45928f2eddfa08ca5b08d7c7f032821f118f784
srensi <srensi@stanford.edu>
2018-04-19 16:12:03 -0700

    Old scripts for importing word vectors

---
+++ code/old_stuff_from_stef.py
@@ -0,0 +1,23 @@
+from gensim.models.keyedvectors import KeyedVectors
+
+w1 = "hypertension"
+w2 = "symptoms"
+# Load word vectors
+word_vectors = KeyedVectors.load_word2vec_format('~/Research/word2vec_stuff/bio-nlp-lab-mancester/Pubmed-w2v.bin',
+                                                 binary=True)
+# test3 = word_vectors.most_similar_cosmul(positive=[w2, w1], topn=20)
+test = word_vectors.most_similar_cosmul(positive=w2)
+test2 = word_vectors.most_similar_cosmul(positive=w1)
+inpt = test + test2
+test3 = word_vectors.most_similar_cosmul(positive=[i[0] for i in inpt], topn=20)
+# test = word_vectors.most_similar(positive=["hypotension", "dehydration", "polyuria", "gout"])
+# test2 = word_vectors.most_similar(positive=["weakness", "dizziness", "urination"], negative=["hypotension", "dehydration"])
+print(test)
+print(test2)
+# print(test3)
+stuff = (set([i[0] for i in test3]) - set([i[0] for i in test])) - set([i[0] for i in test2])
+print(stuff)
+# rev_test = word_vectors.most_similar(positive=list(stuff), negative=[i[0] for i in test])
+# rev_test = word_vectors.most_similar(positive=[i[0] for i in test3])
+# rev_test = word_vectors.most_similar(positive=list(stuff), topn=20)
+# print(rev_test)
