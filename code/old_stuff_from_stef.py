981d9e26c625c2c34dad500a1d5ff868b1599fe5
Charles Akin-David <aakindav@stanford.edu>
2018-04-26 14:44:15 -0700

    step 0

--- code/old_stuff_from_stef.py
+++ code/old_stuff_from_stef.py
@@ -1,5 +1,17 @@
 from gensim.models.keyedvectors import KeyedVectors
+import numpy as np
 
+all_MEDDRA = []
+all_MEDDRA_firest_term = []
+
+# Load MEDDRA terms
+with open('../lasix_meddra.tsv') as file:
+	lines_in_file = file.read().splitlines()
+	for line in lines_in_file:
+		line_list = line.split('\t')
+		meddra_term = line_list[5]
+		all_MEDDRA.append(meddra_term)
+		all_MEDDRA_firest_term.append(meddra_term.split()[0])
 
 # Change to load adverse event words from file
 w1 = "hypertension"
@@ -7,6 +19,23 @@ w2 = "symptoms"
 
 # Load word vectors
 word_vectors = KeyedVectors.load_word2vec_format('../data/PubMed-and-PMC-w2v.bin', binary=True)
+MEDDRA_word_vectors = []
+word_vecs_found_for = []
+no_word_vecs = []
+for word in all_MEDDRA_firest_term:
+	try:
+		word_vec = word_vectors.get_vector(word)
+		MEDDRA_word_vectors.append(word_vec)
+		word_vecs_found_for.append(word)
+	except KeyError, e:
+		no_word_vecs.append(word)
+
+MEDDRA_word_vectors = np.array(MEDDRA_word_vectors)
+
+print word_vecs_found_for
+print no_word_vecs
+print len(word_vecs_found_for), len(no_word_vecs)
+
 
 # Get rid of stuff in quotes
 """
